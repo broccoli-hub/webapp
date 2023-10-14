9096058caf102fa771ee1a7d2fec42e34b04f22a
Alejo Thomas Ortega <alejothomasortega@gmail.com>
2023-10-13 22:43:48 -0300

    refactor: devices and scaffolding

--- src/containers/devices/AirQualityAnalyzer.tsx
+++ src/containers/devices/AirQualityAnalyzer.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 
 import AirQualityChart from './AirQualityChart';
-import DeviceData from '../DeviceData';
-import { generateMockData } from './../../utils/airQualityDataGenerator'
+import DeviceData from '../../components/DeviceData';
+import { generateMockData } from '../../utils/airQualityDataGenerator'
 
 import './AirQualityAnalyzer.css'
 
