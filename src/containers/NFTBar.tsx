2168ea3d219572024cef87748cd3447c8d7e361b
Alejo Thomas Ortega <alejothomasortega@gmail.com>
2023-10-14 00:13:13 -0300

    feat: NFT Tab bar

---
+++ src/containers/NFTBar.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+import "./NFTBar.css";
+
+function NFTBar() {
+  const [selected, setSelected] = useState("NFT name 1");
+
+  return (
+    <div className="segmented-control">
+      <button
+        className={`segment ${selected === "NFT name 1" ? "active" : ""}`}
+        onClick={() => setSelected("NFT name 1")}
+      >
+        NFT name 1
+      </button>
+      <button
+        className={`segment ${selected === "NFT name 2" ? "active" : ""}`}
+        onClick={() => setSelected("NFT name 2")}
+      >
+        NFT name 2
+      </button>
+    </div>
+  );
+}
+
+export default NFTBar;
