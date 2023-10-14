b838a6465d5570dee61cdbe3063bc0d5457d42ac
Alejo Thomas Ortega <alejothomasortega@gmail.com>
2023-10-13 23:44:55 -0300

    refactor: dashboard body look-like

--- src/containers/DashboardBody.tsx
+++ src/containers/DashboardBody.tsx
@@ -1,12 +1,35 @@
 
-import AirQualityAnalyzer from '../components/devices/AirQualityAnalyzer';
-import './DashboardBody.css';
+import { useEffect, useState } from 'react';
 
-const DashboardBody = () => {
+import DeviceComponent from '../components/Device';
+import AirQualityAnalyzer from './devices/AirQualityAnalyzer';
+import Lamp from './devices/Lamp';
+import { generateMockData } from '../utils/airQualityDataGenerator';
+
+import './DashboardBody.css';
+import AddDeviceComponent from './devices/AddDeviceComponent';
+
+const DashboardBody: React.FC = () => {
+    const [data, setData] = useState<any>({});
+
+    useEffect(() => {
+        const mockData = generateMockData();
+        setData(mockData);
+    }, []);
+
     return (
         <div className="body">
             <div className="device-label">My devices</div>
-            <AirQualityAnalyzer />
+            {data?.entities &&
+                <>
+                    <DeviceComponent device={{ type: 'Air Quality Analyzer' }} options={{ isSingle: false }}><AirQualityAnalyzer /></DeviceComponent>
+                    <div className="two-devices-wrapper">
+                        <DeviceComponent device={{ type: 'Light Switch' }} options={{ isSingle: true, isSwitch: true }}><Lamp customName='Custom Name' status='Off'/></DeviceComponent>
+                        {/* <DeviceComponent device={{ type: 'Light Switch' }} options={{ isSingle: true, isSwitch: true }}><Lamp customName='Custom Name' status='On'/></DeviceComponent> */}
+                        <AddDeviceComponent />
+                    </div>
+                </>
+            }
         </div>
     )
 }
