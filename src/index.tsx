fed466d749a805c13e411bf2bf19a4523b8a5b87
Alejo Thomas Ortega <alejothomasortega@gmail.com>
2023-10-14 00:24:08 -0300

    feat: add date adapter for chart.js

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';  // assuming you have this global CSS file
 import App from './App';
 
+import { registry } from 'chart.js';
+import * as dateFnsAdapter from 'chartjs-adapter-date-fns';
+
+registry.add(dateFnsAdapter);
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
