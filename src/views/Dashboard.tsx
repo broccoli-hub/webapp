719176a33c0fc5920c30b324aedfe024b7f883e4
Alejo Thomas Ortega <alejothomasortega@gmail.com>
2023-10-14 00:21:48 -0300

    feat: integrate NFT Bar on dashboard

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,10 +1,13 @@
 import DashboardBody from "../containers/DashboardBody";
 import DashboardHeader from "../containers/DashboardHeader";
 
+import NFTBar from "../containers/NFTBar";
+
 const Dashboard = () => {
     return (
         <>
             <DashboardHeader earningPower={1.21} name='John' totalBalance={213.757}/>
+            <NFTBar />
             <DashboardBody />
         </>
     )
